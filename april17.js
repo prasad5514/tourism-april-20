cbbfd664e9a3dd0753c78371fb95c593a60b11b5
prasad5514 <61958900+prasad5514@users.noreply.github.com>
2020-05-04 19:08:56 +0530

    Add files via upload

--- april17.js
+++ april17.js
@@ -4,15 +4,16 @@ function validate()
 {
     var regexpemail=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
          //var regexppwd=/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/just edited again
-         if((regexpemail.test(email.value))&&(pwd.value!=""))
+         //if((regexpemail.test(email1.value))&&(pwd.value!=""))
+         if(regexpemail.test(email1.value))
          {
-             error1.innerHTML=" email and password is valid";
+             error1.innerHTML=" email is valid";
              error1.style.color="green";
              return true;
         }
          else 
          {
-             error1.innerHTML=" email or password is invalid";
+             error1.innerHTML=" email is invalid";
              error1.style.color="red";
              return false;
       
@@ -28,7 +29,7 @@ var phno=document.getElementById("phn");
 //var cty=document.getElementById("city");
 //var cntry=document.getElementById("country");
 var crpas=document.getElementById("crpass");
-//var cfpas=document.getElementById("cfpass");
+var cfpas=document.getElementById("cfpass");
 //var passpow=document.getElementById("power");
 
 //pattern definitions
@@ -49,6 +50,7 @@ function emailtest()
      if (emlexp.test(email.value)) { return true;}
        else 
        { error2.innerHTML="email is not valid";
+       //eml.style.border="5px solid red";
              return false; }
 }
 
@@ -68,31 +70,60 @@ function passtest()
 {
      if (crpas.value!="") { return true;}
        else 
-       { error2.innerHTML="password is not valid";
+       { error2.innerHTML="password is not given";
              return false; }
 
 }
 
 //password strength
+var regexp1=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([A-za-z0-9]{8,})$/
+var regexp2=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([A-za-z0-9]{5,7})$/
+var regexp3=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([A-za-z0-9]{3,4})$/
 function strength()
 {
-if (crpas.value.length<5) { error2.innerText="password is weak";
-                     error2.style.borderColor="red";
-                     error2.style.color="red";
-                     return false;
+if (regexp1.test(crpas.value)) { error2.innerText="password is strong";
+                     error2.style.borderColor="green";
+                     error2.style.color="green";
+                     return true;
 
                      }
-else if (crpas.value.length>=5 && crpas.value.length<10 ) { error2.innerText="password is moderate";
+else if (regexp2.test(crpas.value)) { error2.innerText="password is moderate";
                      error2.style.borderColor="orange";
                      error2.style.color="orange";
                      return false;
 
 }
-else   { error2.innerText="password is strong";
-                     error2.style.borderColor="green";
-                     error2.style.color="green";
-                     return true;
+else if(regexp3.test(crpas.value))   { error2.innerText="password is weak";
+                     error2.style.borderColor="red";
+                     error2.style.color="red";
+                     return false;
   }
+else   { error2.innerText="Too weak,need one capital letter and one small letter and one digit atleast";
+                     error2.style.borderColor="red";
+                     error2.style.color="red";
+                     return false;
+  }
+}
+function samepass()
+{
+     if(cfpas.value=="")
+     {
+          error2.innerHTML="confirm password not given";
+          error2.style.color="brown";
+          return false;
+     }
+     else if((crpas.value)==(cfpas.value))
+     {
+          error2.innerHTML="strong password and successful";
+          error2.style.color="green";
+          return true;
+     }
+     else
+     {
+          error2.innerHTML="Different password";
+          error2.style.color="red";
+          return false;
+     }
 }
 
 
